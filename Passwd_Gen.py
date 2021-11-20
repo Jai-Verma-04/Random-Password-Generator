af98e3e59a935e86aa040d654c2b49efeb4829cb
Jai Verma <94690866+Jai-Verma-04@users.noreply.github.com>
2021-11-20 13:09:15 +0530

    Add files via upload

---
+++ Passwd_Gen.py
@@ -0,0 +1,34 @@
+from random import choice
+
+def main():
+    try:
+        length = int(input('Enter Length of the password: '))
+
+        symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
+
+        letters =  ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
+                    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 
+                    'w', 'x', 'y', 'z']
+
+        numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+        passwd = ''
+        options = [letters, numbers, symbols]
+        for i in range(0, length):   
+             opt = choice(choice(options))
+             opt = str(opt)
+             passwd+=opt
+        print('your password: {}'.format(passwd))
+        def again_():
+            again = input('Do you want to generate another password? [Y/N]').lower()
+            if again == 'y':
+                main()
+            elif again=='n':
+                return False
+            else:
+                again_()
+        again_()
+    except ValueError:
+        print('Enter a valid length')
+        main()
+main()
