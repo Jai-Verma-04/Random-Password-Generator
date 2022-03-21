4cccdbf4de8af76cb744fe8a072a4f648da565bc
Jai Verma <94690866+Jai-Verma-04@users.noreply.github.com>
2022-03-21 11:02:42 +0530

    Update Passwd_Gen.py

--- Passwd_Gen.py
+++ Passwd_Gen.py
@@ -6,14 +6,16 @@ def main():
 
         symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
 
-        letters =  ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
+        small_letters =  ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
                     'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 
                     'w', 'x', 'y', 'z']
-
+        
+        caps_letters = [i.upper() for i in small_letters]
+        
         numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
 
         passwd = ''
-        options = [letters, numbers, symbols]
+        options = [small_letters, caps_letters, numbers, symbols]
         for i in range(0, length):   
              opt = choice(choice(options))
              opt = str(opt)
